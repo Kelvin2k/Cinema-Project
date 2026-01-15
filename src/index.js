0a8e8e1dab806719ca43ccd30b6026ba4eafa71a
Duc Hoi Chau <dcha0120@student.monash.edu>
2026-01-16 05:09:22 +1100

    Remove flowbite import causing require() error

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./redux/configStore";
 import "@tailwindplus/elements";
-import "flowbite";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
