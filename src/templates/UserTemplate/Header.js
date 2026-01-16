1d9319b8c0d108da215d15b5f845bce6068519a4
Duc Hoi Chau <dcha0120@student.monash.edu>
2026-01-16 20:42:32 +1100

    fix: update Header logo href from # to /

--- src/templates/UserTemplate/Header.js
+++ src/templates/UserTemplate/Header.js
@@ -37,7 +37,7 @@ const Header = () => {
         className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
       >
         <div className="flex lg:flex-1">
-          <a href="#" className="-m-1.5 p-1.5">
+          <a href="/" className="-m-1.5 p-1.5">
             <span className="sr-only">Your Company</span>
             <img
               src="https://static.vecteezy.com/system/resources/previews/022/580/623/non_2x/movie-media-letter-logo-design-illustration-free-vector.jpg"
